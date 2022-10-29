8a788b12483522050472f992cc09d11011cc0dde
Ryan Foust <r.foust1@outlook.com>
2022-10-29 17:29:09 -0400

    get_to_work.py Create example task for debugging purposes Read personal API token from file Open Todoist after

--- get_to_work/get_to_work.py
+++ get_to_work/get_to_work.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
+from todoist_api_python.api import TodoistAPI
+import subprocess
+
 debug = True
+
+with open("_todoist_token.txt", "r") as TDI_Token:
+    token = TDI_Token.read()
+    api = TodoistAPI(token)
 
 
 def main():
     if debug:
         print("Starting main")
+
+    if debug:
+        print("Made example task")
+        api.add_task(content="Example", project_id=2291523269)
+    subprocess.Popen('/snap/bin/todoist')
 
 
 if __name__ == '__main__':
