0b2a50608506c355d6f550213030d8ebe902e43f
Ryan Foust <r.foust1@outlook.com>
2022-11-02 17:30:21 -0400

    Stubbed open functions, redid checkbox creation and packing process

--- get_to_work/get_to_work.py
+++ get_to_work/get_to_work.py
@@ -5,6 +5,14 @@ import json
 import subprocess
 import webbrowser
 
+def open_website():
+    # webbrowser.open([url])
+    print("open_website called")
+
+def open_app():
+    # subprocess.Popen([path])
+    print("open_app called")
+
 debug = False
 stage = Tk()
 stage.title("Get to Work Shortcut")
@@ -30,27 +38,33 @@ def main():
         print(apps_and_websites)
         print(type(apps_and_websites))
 
+    elements = []
     # for each element in json data
     for app in apps_and_websites:
         if debug:
             print(app['name'])
 
-        # make each checkbox
-        app_check_button = Checkbutton(stage, text=app['name'])
-        app_check_button.pack()
+        # make each checkbox, depending on whether the
+        if app['is_website']:
+            app_check_button = Checkbutton(stage, text=app['name'], command=open_website)
+            elements.append(app_check_button)
+        else:
+            app_check_button = Checkbutton(stage, text=app['name'], command=open_app)
+            elements.append(app_check_button)
 
+    for elem in elements:
+        elem.pack()
+
+    # make continue button
     continue_button = Button(stage, text="Continue")
-    exit_button = Button(stage, text="Exit", command=stage.destroy)
     continue_button.pack()
+
+    # make exit button
+    exit_button = Button(stage, text="Exit", command=stage.destroy)
     exit_button.pack()
 
+    # start GUI portion
     stage.mainloop()
-
-    if debug:
-        print("Made example task")
-        api.add_task(content="Example", project_id=2291523269)
-    # subprocess.Popen('/snap/bin/todoist')
-    # webbrowser.open("https://calendar.google.com/calendar/u/0/r")
 
 
 if __name__ == '__main__':
